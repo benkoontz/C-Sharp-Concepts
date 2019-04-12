e11d9eb192bf0e16398ef3a3ad9a441cb47fba61
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-04-12 11:12:07 -0700

    Create Program.cs

---
+++ Day 02/Datatypes/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "hello";
+            char grade = 'A';
+            int age = 30;
+           // float, double, decimal         // least accurate to most accurate
+            double gpa = 3.3;
+            bool isMale = true;
+
+            Console.ReadLine();
+        }
+    }
+}
