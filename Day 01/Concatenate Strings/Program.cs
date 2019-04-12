1746c7dee3b5f5555768f1d7d9d92d6ed1942a19
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-04-12 11:07:16 -0700

    Create Program.cs

---
+++ Day 01/Concatenate Strings/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ben = "ben";
+            int characterage = 24;
+
+            Console.WriteLine("My name is " + ben);
+            Console.WriteLine("I'm " + characterage + " years old");
+            Console.WriteLine("I like pie");
+
+            Console.ReadLine();
+        }
+    }
+}
