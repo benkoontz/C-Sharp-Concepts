d6cc5d0da0367d8ed54723e5c4bf78ea82f1c228
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-04-13 11:45:29 -0700

    Create Program.cs

---
+++ Day 20/Comments/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // this is a single line comment
+            /*  
+             *  
+             *  
+             *  
+             this is a multiline comment
+             */
+            Console.WriteLine("Comments are fun");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
