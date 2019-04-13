db7021032cc7c83bb8e77246ebb20550eaa98bcd
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-04-13 09:45:29 -0700

    Create Program.cs

---
+++ Day 04/Numbers, Math operators/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // math operators +, -, *, /, %
+            // increment ++
+            // decrement --
+            Console.WriteLine(Math.Abs(-40)); // prints 40
+            Console.WriteLine(Math.Pow(3, 2)); // 3^2 = 9
+            Console.WriteLine(Math.Max(40, 41)); // gives max number
+            Console.WriteLine(Math.Round(4.53));
+
+            Console.ReadLine();
+        }
+    }
+}
